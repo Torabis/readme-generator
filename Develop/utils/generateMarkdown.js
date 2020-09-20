081fbd4d22a99c7c10cdedbec5662af6fdbc19e1
Torabis <sadihome@gmail.com>
2020-09-20 02:38:27 -0700

    create and export generateMarkdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  readme = `# ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  *[Installation](#installation)  
+  *[Usage](#usage)  
+  *[Contributing](#contributing)  
+  *[License](#license)  
+  *[Tests](#tests)  
+  *[Questions](#questions)  
 
-`;
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## License
+  ${data.license}  
+ 
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Email: ${data.email}  
+  GitHub: ${data.github}  
+  `;
+
+  return readme;
 }
 
 module.exports = generateMarkdown;
